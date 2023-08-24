581fae6e19aa308d2a285eb713be69944090d2f7
pinzdesign <tyrael2101@hotmail.com>
2023-08-24 10:30:15 +0200

    msg

---
+++ main.js
@@ -0,0 +1,21 @@
+// Opgave 2A
+
+// titlen
+let overskrift = document.querySelector("#overskrift");
+console.log(overskrift.textContent);
+
+// undertitlen
+let underskrift = document.querySelector("#underskrift");
+console.log(underskrift.textContent);
+
+// undertitlen
+let parOne = document.querySelector("#parOne");
+console.log(parOne.textContent);
+
+// undertitlen
+let parTwo = document.querySelector("#parTwo");
+console.log(parTwo.innerHTML);
+
+// undertitlen
+let footer = document.querySelector("#footer");
+console.log(footer.innerHTML);
